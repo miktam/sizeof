d2e3ef93d3fef87ae78bfdb16ac8ea80db3bf507
Denis Sedchenko <denis0051@gmail.com>
2019-01-11 04:30:03 +0100

    Code style improvements (#25)
    * code refactoring
    * fix typo
    * add buffer size check

--- index.js
+++ index.js
@@ -5,6 +5,32 @@
 var ECMA_SIZES  = require('./byte_size');
 var Buffer = require('buffer').Buffer;
 
+function sizeOfObject(object) {
+  if (object == null) {
+    return 0;
+  }
+
+  var bytes = 0;
+  for (var key in object) {
+    if(!Object.hasOwnProperty.call(object, key)) {
+      continue;
+    }
+
+    bytes += sizeof(key);
+    try {
+      bytes += sizeof(object[key]);
+    } catch (ex) {
+      if(ex instanceof RangeError) {
+        // circular reference detected, final result might be incorrect
+        // let's be nice and not throw an exception
+        bytes = 0;
+      }
+    }
+  }
+
+  return bytes;
+}
+
 /**
  * Main module's entry point
  * Calculates Bytes for the provided parameter
@@ -12,39 +38,22 @@ var Buffer = require('buffer').Buffer;
  * @returns {*}
  */
 function sizeof(object) {
-    if (object !== null && typeof (object) === 'object') {
-      if (Buffer.isBuffer(object)) {
-        return object.length;
-      }
-      else {
-        var bytes = 0;
-        for (var key in object) {
+    if (Buffer.isBuffer(object)) {
+      return object.length;
+    }
 
-          if(!Object.hasOwnProperty.call(object, key)) {
-            continue;
-          }
-
-          bytes += sizeof(key);
-          try {
-            bytes += sizeof(object[key]);
-          } catch (ex) {
-            if(ex instanceof RangeError) {
-              // circular reference detected, final result might be incorrect
-              // let's be nice and not throw an exception
-              bytes = 0;
-            }
-          }
-        }
-        return bytes;
-      }
-    } else if (typeof (object) === 'string') {
-      return object.length * ECMA_SIZES.STRING;
-    } else if (typeof (object) === 'boolean') {
-      return ECMA_SIZES.BOOLEAN;
-    } else if (typeof (object) === 'number') {
-      return ECMA_SIZES.NUMBER;
-    } else {
-      return 0;
+    var objectType = typeof(object);
+    switch (objectType) {
+      case 'string':
+        return object.length * ECMA_SIZES.STRING;
+      case 'boolean':
+        return ECMA_SIZES.BOOLEAN;
+      case 'number':
+        return ECMA_SIZES.NUMBER;
+      case 'object':
+        return sizeOfObject(object);
+      default:
+        return 0;
     }
 }
 
