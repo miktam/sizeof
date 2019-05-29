0031aee1fda35faa1ba27cc6f28e2b0ff4cb3495
Milan Velebit <velebit@accusys.co>
2019-05-29 19:17:21 +0200

    Switched param type to generic

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,4 @@
  * Calculates the approximate number of bytes that the provided object holds.
  * @param object
  */
-export declare function sizeof(object: object): number
+export declare function sizeof<T>(object: T): number;
