94509589e324cc48ccbb4f59a5c15cd10cc8517f
Milan Velebit <velebit@accusys.co>
2019-05-29 19:14:46 +0200

    Added a docblock

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,5 @@
-export declare function sizeof(object: object): number;
+/**
+ * Calculates the approximate number of bytes that the provided object holds.
+ * @param object
+ */
+export declare function sizeof(object: object): number
